a6d994fce23c8776d1e36d0c194074e657804584
dengfan <dengfan@diandian-tech.com>
2017-06-28 10:28:32 +0800

    css module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import './style.postcss';
+import style from './style.postcss';
 import a from './a.js'
 console.log('hello postcsss');
+document.getElementById('app').className = style.foo;
+console.info(style.foo);
 a();
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,15 @@ module.exports = {
                 test:/\.postcss$/,
                 use:ExtractTextPlugin.extract({
                     fallback:'style-loader',
-                    use:['css-loader','postcss-loader']
+                    use:[
+                        {
+                            loader:'css-loader',
+                            options: {
+                                modules : true, 
+                            }
+                        },
+                        'postcss-loader'
+                    ]
                 }),
                 exclude: /node_modules/
             }
